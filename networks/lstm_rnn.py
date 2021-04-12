2b6f4d729e6808cd6d7810978f645e791ee0bebd
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-04-12 09:52:54 -0400

    Updated ODE_RNN

--- networks/lstm_rnn.py
+++ networks/lstm_rnn.py
@@ -7,7 +7,26 @@ from crossbar.crossbar import crossbar
 
 class LSTM_RNN(nn.Module):
 
-    def __init__(self, params):
+    def __init__(self, input_layer_size, hidden_layer_size, output_layer_size, device_params):
 
         super(LSTM_RNN, self).__init__()
 
+        self.input_layer_size = input_layer_size
+        self.hidden_layer_size = hidden_layer_size
+        self.output_layer_size = output_layer_size
+
+        self.cb = crossbar(device_params)
+
+        self.lstm = nn.LSTM(input_layer_size, hidden_layer_size, num_layers=1, batch_first=True)
+        self.linear = Linear(hidden_layer_size, output_layer_size, self.cb)
+    
+    def forward(self, x):
+        output, hidden = self.lstm(x)
+        output = output[:, 1, :]
+        output = self.linear(torch.relu(output))
+        return output
+
+    def use_cb(self, state):
+        self.linear_in.use_cb(state)
+        self.linear_hidden.use_cb(state)
+        self.solve.use_cb(state)
--- networks/ode_rnn.py
+++ networks/ode_rnn.py
@@ -2,18 +2,26 @@
 import torch
 import torch.nn as nn
 
+from torchdiffeq import odeint
+
 from utils.linear import Linear
 from crossbar.crossbar import crossbar
 
 class ODE_Func(nn.Module):
 
-    def __init__(self, params):
+    def __init__(self, hidden_layer_size, cb):
         super(ODE_Func, self).__init__()
+        self.linear = Linear(hidden_layer_size, hidden_layer_size, cb)
+        self.nonlinear = nn.Tanh()
+
+    def forward(self, t, x):
+        out = self.nonlinear(self.linear(x))
 
 class ODE_RNN(nn.Module):
 
-    def __init__(self, params):
-
+    def __init__(self, input_size, hidden_layer_size, output_size, device_params):
+        
         super(ODE_RNN, self).__init__()
 
-        
+        self.cb = crossbar(device_params)
+
