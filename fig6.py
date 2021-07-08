d0a1da6619fc605c05e121ac8d04eabcff159b93
ricohasgithub <ricozhuthegreat@hotmail.com>
2021-07-08 16:03:44 -0400

    Bug Patch

--- fig6.py
+++ fig6.py
@@ -144,12 +144,13 @@ def get_average_gru_performance(iters, epochs, device_params):
 
     return losses_gru, output
 
-def get_average_gru_performance(iters, epochs, device_params, data_gen):
+def get_average_gru_performance_datagen(iters, epochs, device_params, data_gen):
 
     model = GRU_RNN_autogen(2, 6, 2, device_params)
     losses_gru, output = gru_rnn_autogen_train(model, data_gen, epochs)
 
     ax = plt.axes(projection='3d')
+    ax.set_title("GRU-RNN Output")
     ax.plot3D(output[0], output[1], output[2], color="black", linewidth=1.5)
 
     d1, d2, d3 = data_gen.y[0, :].squeeze(), data_gen.y[1, :].squeeze(), data_gen.x.squeeze()
@@ -171,6 +172,14 @@ def get_average_performance_datagen(iters, epochs, data_gen, device_params, meth
 
         for j in range(len(loss)):
             loss_avg[j] += loss[j]
+
+        ax = plt.axes(projection='3d')
+        ax.set_title("ODE-RNN Output")
+        ax.plot3D(output[0], output[1], output[2], color="black", linewidth=1.5)
+
+        d1, d2, d3 = data_gen.y[0, :].squeeze(), data_gen.y[1, :].squeeze(), data_gen.x.squeeze()
+        ax.plot3D(data_gen.true_x, data_gen.true_y, data_gen.true_z, 'gray')
+        ax.scatter3D(d1, d2, d3, 'gray')
 
         print('Iter {:04d}'.format(i))
 
@@ -275,15 +284,14 @@ def graph_model_difference(iters, epochs, device_params, method, time_steps):
     # Get ODE-RNN performance
     loss_ode = get_average_performance_datagen(iters, epochs, data_gen, device_params, method, time_steps)
     # Get GRU-RNN performance
-    loss_gru = get_average_gru_performance(iters, epochs, device_params, data_gen)
+    loss_gru = get_average_gru_performance_datagen(iters, epochs, device_params, data_gen)
 
     # Plot loss history and average loss
     fig, ax_loss = plt.subplots()
     fig.suptitle('Average MSE Loss')
 
-    for i in range(iters):
-        ax_loss.plot(list(range(epochs)), loss_ode[i], color="blue", linewidth=1)   
-        ax_loss.plot(list(range(epochs)), loss_gru[i], color="red", linewidth=1)     
+    ax_loss.plot(list(range(epochs)), loss_ode, color="blue", linewidth=1)   
+    ax_loss.plot(list(range(epochs)), loss_gru, color="red", linewidth=1)    
 
     fig.savefig('./output/training_avg.png', dpi=600, transparent=True)
 
